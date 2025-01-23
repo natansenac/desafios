744b44d3cf5ea2cb707ef9a29536fe493b8fa593
NATAN VELASCO PEDROSO <natan.vpedroso@senacsp.edu.br>
2025-01-22 21:51:26 -0300

    ex05

--- desafio02/obj/Debug/net8.0/desafio02.AssemblyInfo.cs
+++ desafio02/obj/Debug/net8.0/desafio02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desafio02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ca6ff52e5894dfb4d807c3b2e39a7a7836d9a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("desafio02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desafio02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ desafio05/Program.cs
@@ -0,0 +1,16 @@
+﻿// Escreva um programa que solicita a um usuário a quantidade em litros abastecidos em um automóvel. Em seguida, o programa deve solicitar que o usuário informe a quantidade de quilômetros percorridos até o término do tanque. O programa deve exibir a quantidade de quilômetros por litro.
+
+
+// Solicitar a quantidade de litros abastecidos
+Console.WriteLine("Quantidade de litros abastecidos");
+double litrosAbestecidos = double.Parse(Console.ReadLine());
+
+// Solicitar a quantidade de quilômetros percorridos
+Console.WriteLine("Quantidade de quilômetros percorridos");
+double kmPercorridos = double.Parse(Console.ReadLine());
+
+// Calcular a quantidade de quilômetros por litro
+double kmPorLitro = kmPercorridos / litrosAbestecidos;
+
+// Resultado
+Console.WriteLine($"O consumo do automóvel é de {kmPorLitro} quilômetros por litro");
